b8d907da7bbb8d29070e30a6ffc723d0490b47e6
G4os <gustavorellana700@gmail.com>
2025-10-05 00:43:14 -0300

    se hicieron los cambios para que funcionen los checkbox y el boton borrar

--- js/script.js
+++ js/script.js
@@ -1,17 +1,58 @@
-let tareas = []
+let tareas = [];
 
 const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
-const tasksList = document.getElementById('tasksList')
-const openTasksCount = document.getElementById('openTasksCount')
+const tasksList = document.getElementById('tasksList');
+const openTasksCount = document.getElementById('openTasksCount');
+const closeTasksCount = document.getElementById('closeTasksCount');
+
+
+function renderHtml(){
+    tasksList.innerHTML = '';
+    let abiertas = 0;
+    let cerradas = 0;
+    tareas.forEach((tarea, i) => {
+        const li = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = tarea.completada;
+        checkbox.addEventListener('change', ()=> {
+            tarea.completada = !tarea.completada;
+            renderHtml();
+        });
+    
+    const span = document.createElement('span');
+    span.textContent = tarea.descipcion;
+
+    const btn = document.createElement('button');
+    btn.textContent = 'Borrar';
+    btn.addEventListener('click', () => {
+        tareas.splice(i, 1);
+        renderHtml();
+    });
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(btn);
+    tasksList.appendChild(li);
+
+    if (!tarea.completada){
+        abiertas++;
+    } else {
+        cerradas++;
+    }
+    });
+    openTasksCount.textContent = abiertas;
+    closeTasksCount.textContent = cerradas;
+}
+
 
 
 addTaskBtn.addEventListener('click' , () => {
     const desc = taskInput.value.trim();
     if (desc) {
         tareas.push({descipcion: desc , completada: false});
-        console.log(tareas);
-        taskInput.value = ' ';
+        taskInput.value = '';
+        renderHtml();
     }
-})
+});
 
